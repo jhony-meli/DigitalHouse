fdc6bf852fca7f4a9f47e54a8a054596bfc8ec1a
jhony-meli <jhony.zuim@mercadolivre.com>
2021-08-28 15:57:46 -0300

    novas aulas

---
+++ src/com/exemplos/Main.java
@@ -0,0 +1,17 @@
+package com.exemplos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int valor;
+
+        valor = soma(10, 20);
+        System.out.println("A soma e : " + valor);
+    }
+
+    static int soma(int i, int i1) {
+        int resultado = i + i1;
+        return resultado;
+    }
+}
