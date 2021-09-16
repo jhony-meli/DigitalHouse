d4f5150c015e40271d73933c36a853c2cc153879
jhony-meli <jhony.zuim@mercadolivre.com>
2021-09-16 18:45:25 -0300

    atualizacao

---
+++ AtividadeIndividual/src/com/company/exercicio5/Cliente.java
@@ -0,0 +1,85 @@
+package com.company.exercicio5;
+
+public class Cliente {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+
+
+    public Cliente(String nome, String cpf, String endereco, String email, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public void criarCliente(Cliente cliente){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+
+}
--- AtividadeIndividual/src/com/company/exercicio5/Main.java
+++ AtividadeIndividual/src/com/company/exercicio5/Main.java
@@ -1,8 +1,19 @@
 package com.company.exercicio5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Cliente cliente1 = new Cliente("Jhony", "321.123.456-54","Rua Alvorada","jhony@mercadolivre.com","119876543");
+
+        List<Cliente> listClientes = new ArrayList<>();
+
+        if (cliente1.getNome().length() > 2 && cliente1.getNome().length() < 51){
+            listClientes.add(cliente1);
+        }
+        System.out.println(listClientes);
     }
 }
